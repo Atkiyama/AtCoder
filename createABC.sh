a7e078f2cf789655f3b23746286643ee04c2b57f
Atkiyama <thisisatkiyama@gmail.com>
2023-09-20 09:03:33 +0900

    ABC用スクリプト追加

---
+++ createABC.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 引数の数が正しいか確認
+if [ $# -ne 1 ]; then
+    echo "使用法: $0 <ディレクトリ名>"
+    exit 1
+fi
+
+# 引数からディレクトリ名を取得
+dir_name="$1"
+
+# ディレクトリを作成
+mkdir "$dir_name"
+
+# ディレクトリが正常に作成されたか確認
+if [ $? -eq 0 ]; then
+    echo "ディレクトリ $dir_name が正常に作成されました。"
+else
+    echo "ディレクトリの作成中にエラーが発生しました。"
+    exit 1
+fi
+
+# main.pyからa.py、b.py、c.py、d.pyをコピー
+cp "main.py" "$dir_name/$dir_name _a.py"
+cp "main.py" "$dir_name/$dir_name _b.py"
+cp "main.py" "$dir_name/$dir_name _c.py"
+cp "main.py" "$dir_name/$dir_name _d.py"
+
+# ファイルが正常にコピーされたか確認
+if [ $? -eq 0 ]; then
+    echo "ファイル a.py, b.py, c.py, d.py が正常にコピーされました。"
+else
+    echo "ファイルのコピー中にエラーが発生しました。"
+fi
--- main.py
+++ main.py
@@ -26,46 +26,8 @@ from collections import defaultdict
 def main():
     
     N=int(input())
-    for i in range(1 <<N):
-        st=""
-        for j in range(N-1,-1,-1):
-            if(i & (1 <<j) ==0):
-                st+="("
-            else:
-                st+=")"
-        if judge(st):
-            print(st)
-        
-def judge(st):
-    front=0
-    back=0
-    for i in st:
-        if i=="(":
-            front+=1
-        else:
-            back+=1
-        if front<back:
-            return False
-    if front==back:
-        return True
-    else:
-        return False
-        
     
         
-    
 
-def solve(M,A,K,L,N):
-    count=0
-    pre = 0
-    for i in range(1,N+1):
-        if A[i]-pre>=M and L-A[i]>=M:
-            count+=1
-            pre=A[i]
-        
-    #print(count,end=",")
-    # print(count>K,end=",")
-    
-    return count >=K
 if __name__ == "__main__":
     main()
