7f365a8bf489cb65ba6383de8f9805cd46081535
Atkiyama <thisisatkiyama@gmail.com>
2023-10-05 17:24:46 +0900

    322復習完了

--- 322/322E.py
+++ 322/322E.py
@@ -25,59 +25,77 @@ from collections import defaultdict
 INF = 10 ** 18
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-dxy=[(1,0),(0,1),(-1,0),(0,-1)]
-C=[]
-A=[]
-def main():
-    N,K,P=map(int,input())
-    for i in range(N):
-        tmp=list(map(int,input().split()))
-        C.append(tmp[0])
-        a=[]
-        for j in range(1,K+1):
-           a.append[tmp[j]]
-        A.append(a) 
-    
-    ans=[]
-    for i in range(1<<N):
-        param=[0 for _ in range(K)]
-        for j in range(N):
-            if (i &(1<<j)):
-                for k in range(K):
-                    param[k]+=A[j][k]
-                    
-        flag=True
-        for j in range(K):
-            if param[j]<P:
-                flag=False
-        if flag
-        
-                
-        
+dxy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+C = []
+A = []
 
-def swap(A,i,j):
-    tmp=A[i]
-    A[i]=A[j]
-    A[j]=tmp
-    
+'''
+dp[i番目の案まで使う][パラメータがP(1,2,,,,,)になる]
+を更新し続ける
+表の全てが埋まるわけではないのが注意点
+
+'''
+
+
+def main():
+    N, K, P = map(int, input().split())
+    for i in range(N):
+        tmp = list(map(int, input().split()))
+        C.append(tmp[0])
+        a = []
+        for j in range(1, K+1):
+            a.append(tmp[j])
+        A.append(a)
+    dp = defaultdict(lambda: INF)
+
+    dp[tuple([0]*K)] = 0
+    for i in range(N):
+        items = list(dp.items())
+        for k, v in items:
+            tmp = list(k)
+            for j in range(K):
+                tmp[j] += A[i][j]
+                if tmp[j] > P:
+                    tmp[j] = P
+            tmp = tuple(tmp)
+            dp[tmp] = min(dp[tmp], v+C[i])
+
+    ans = dp[tuple([P]*K)]
+    # print(dp)
+    if ans == INF:
+        print(-1)
+    else:
+        print(ans)
+
+
+def swap(A, i, j):
+    tmp = A[i]
+    A[i] = A[j]
+    A[j] = tmp
+
+
 def basezero(num):
     return num-1
 
-def checkIndex(list,i):
-    length=len(list)
-    
-    if 0<=i<length:
+
+def checkIndex(list, i):
+    length = len(list)
+
+    if 0 <= i < length:
         return True
     else:
         return False
 
-def checkIndex2(list,i,j):
-    H=len(list)
-    W=len(list[0])
-    if 0<=i<H and 0<=j<W:
+
+def checkIndex2(list, i, j):
+    H = len(list)
+    W = len(list[0])
+    if 0 <= i < H and 0 <= j < W:
         return True
     else:
         return False
+
+
 class UnionFind():
     def __init__(self, n):
         self.n = n
@@ -127,12 +145,19 @@ class UnionFind():
 
     def __str__(self):
         return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+
+
 def yes():
     print("Yes")
+
+
 def no():
     print("No")
+
+
 def minusOne():
     print(-1)
-    
+
+
 if __name__ == "__main__":
     main()
